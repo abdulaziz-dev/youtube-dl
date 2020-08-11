74984813f5a5890c80ff30b69d4bff9e71358dab
abdulaziz-dev <60459153+abdulaziz-dev@users.noreply.github.com>
2020-08-11 19:33:45 +0500

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,9 @@ echo($youtube_video);
  */
 function getDirectUrl($youtube_video) {
   // lets build command to get direct url via youtube-dl
-  $video_json_command = YOUTUBE_DL.'--rm-cache-dir -g '.$youtube_video;
+  $video_json_command = YOUTUBE_DL.' -g '.$youtube_video;
+ $clcmd = YOUTUBE_DL.' --rm-cache-dir';
+ shell_exec($clcmd);
   // get url
   $direct_url = shell_exec($video_json_command);
 // remove any possible white spaces
